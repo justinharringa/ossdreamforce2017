7c2570978c77c88c9d92680c782eff9eec2f33dc
Justin Harringa <jharringa@salesforce.com>
2017-10-26 10:32:56 -0700

    Add jenkins user in script

--- jenkins/build_and_start.sh
+++ jenkins/build_and_start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+echo "Adding Jenkins user/group if not already added"
+user=jenkins
+group=jenkins
+uid=1000
+gid=1000
+groupadd -g ${gid} ${group}     && useradd -d "/var/lib/jenkins" -u ${uid} -g ${gid} -m -s /bin/bash ${user}
+
+echo "Building oss-argus-jenkins Docker container"
 docker build -t oss-argus-jenkins .
+echo "Running oss-argus-jenkins"
 docker-compose up -d --force-recreate
