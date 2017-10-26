d5e5ed0860d596fe84be314c7c99940fb49652d7
Justin Harringa <jharringa@salesforce.com>
2017-10-26 10:36:18 -0700

    Create directory and assign permissions

--- jenkins/build_and_start.sh
+++ jenkins/build_and_start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-echo "Adding Jenkins user/group if not already added"
+echo "Creating /var/lib/jenkins and adding Jenkins user/group if not already added"
 user=jenkins
 group=jenkins
 uid=1000
 gid=1000
+sudo mkdir -p /var/lib/jenkins
 sudo groupadd -g ${gid} ${group} && sudo useradd -d "/var/lib/jenkins" -u ${uid} -g ${gid} -m -s /bin/bash ${user}
+sudo chown jenkins:jenkins /var/lib/jenkins
 
 echo "Building oss-argus-jenkins Docker container"
 docker build -t oss-argus-jenkins .
