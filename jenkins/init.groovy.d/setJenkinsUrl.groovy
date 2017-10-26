0490004154df9131a112a0d09d5120495b29bbf3
Justin Harringa <jharringa@salesforce.com>
2017-10-26 11:22:28 -0700

    Add diagnostics to init.groovy.d

--- jenkins/init.groovy.d/setJenkinsUrl.groovy
+++ jenkins/init.groovy.d/setJenkinsUrl.groovy
@@ -2,6 +2,9 @@ import jenkins.model.JenkinsLocationConfiguration
 
 def url = System.getenv('MAIN_JENKINS_URL')
 if (url) {
+    println "Found MAIN_JENKINS_URL var, setting to $url"
     def jenkinsLocation = jenkins.model.JenkinsLocationConfiguration.get()
     jenkinsLocation.setUrl(url)
+} else {
+    println "Didn't find MAIN_JENKINS_URL var, skipping config"
 }
